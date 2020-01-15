663c61fcea4a5aa3e980a3e99f56dc01cdd9b890
Madhurjya Das <madhurjyakumardas@gmail.com>
2020-01-16 02:54:18 +0530

    changes made

--- Array/sort_minSwaps.py
+++ Array/sort_minSwaps.py
@@ -1,5 +1,19 @@
 def MininmumSwaps(arr):
+    n = len(arr)
+    temp = [None]*(n+1)
+    for i, val in enumerate(arr):
+        temp[val] = i
+        i = i+1
     
+    count = 0
+    for i in range(n):
+        if arr[i] != (i+1):
+            t = arr[i]
+            arr[i] = i+1
+            arr[temp[i+1]] = t
+            temp[t] = temp[i+1]
+            count = count+1
+    return count
 
 
 if __name__ == "__main__":
