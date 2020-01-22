c4d0fc09ce85f2e43dc3ce66bed196abbb1dde7e
Madhurjya Das <madhurjyakumardas@gmail.com>
2020-01-22 12:40:37 +0530

    updated insertion sort

--- Array/insertion_sort.py
+++ Array/insertion_sort.py
@@ -1,23 +1,21 @@
-def insertion_sort(arr):
-    length = len(arr)
-    for i in range(1, length):
-        key = arr[i]
-        j = i - 1
-        while j >= 0 and key < arr[j]:
+def insertionSort(arr):
+    size = len(arr)
+    for i in range(1, size):
+        j = i-1
+        temp = arr[i]
+        while j >= 0 and temp < arr[j]:
             arr[j+1] = arr[j]
             j = j-1
-        arr[j+1] = key
+        arr[j+1] = temp
+
 
 if __name__ == "__main__":
-    arr = [4, 3, 2, 10, 12, 1, 5, 6]
-    print("\nThe Array Elements are ->", end=" ")
-    for i in range(len(arr)):
-        print(arr[i], end=" ")
+    size = int(input("\nEnter the Size: "))
+    arr = list(map(int, input("Enter the Array Elements: ").split()))[:size]
     
-    insertion_sort(arr)
+    insertionSort(arr)
 
-    print("\nThe Sorted Array Elements are ->", end=" ")
-    for i in range(len(arr)):
-        print(arr[i], end=" ",)
-    
-    print()
+    print("Sorted Array Elements are: ", end = " ")
+    for i in range(size):
+        print(arr[i], end = " ")
+    print("\n")
