deef408ce24bc8a9a745601b92244a51f0745c3a
Madhurjya Das <madhurjyakumardas@gmail.com>
2020-02-26 10:58:14 +0530

    Updated LargestSmallest_Corresponding.py program

--- Array/LargestSmallest_Corresponding.py
+++ Array/LargestSmallest_Corresponding.py
@@ -16,14 +16,22 @@ def LargestSmallest(arr, n):
         res[x] = sort_arr[i]
         x = x+2
     
-    print(res)
+    return res
 
 
 if __name__ == "__main__":
-    print("\n")
-    arr = [5, 10, 15, 2, 3, 9, 7, 8, 11]
-    n = len(arr)
-    LargestSmallest(arr, n)
+    # print("\n")
+    # arr = [5, 10, 15, 2, 3, 9, 7, 8, 11]
+    # n = len(arr)
+    # LargestSmallest(arr, n)
+    # print("\n")
+
+    n = int(input("\nEnter the Array Size: "))
+    arr = list(map(int, input("Enter the Array Elements: ").split()))[:n]
+    res = LargestSmallest(arr, n)
+    print("\nSorted List: ", end = " ")
+    for i in range(len(res)):
+        print(res[i], end = " ")
     print("\n")
 
 
