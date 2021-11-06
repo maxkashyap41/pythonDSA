580ec8fb52d3154aa3f5b1aa88ee6194ffc3692a
Madhurjya Das <madhurjyakumardas@gmail.com>
2021-11-07 00:57:22 +0530

    LargestSmallest_Corresponding modified with some minimal ops

--- Array/LargestSmallest_Corresponding.py
+++ Array/LargestSmallest_Corresponding.py
@@ -1,3 +1,7 @@
+# sort the largest number into descending and smallest number in ascending.
+# Example: 37 12 33 9 43 19 20 -> 43 9 37 12 33 19 20
+
+
 def LargestSmallest(arr, n):
     sort_arr = sorted(arr)
     length = len(sort_arr)
@@ -19,6 +23,28 @@ def LargestSmallest(arr, n):
     return res
 
 
+
+def LargeSmall2(arr, n):
+    arr.sort()
+    new_arr = [-1 for _ in range(n)]
+    print(new_arr)
+
+
+    mid = n // 2
+    j = 0
+    for i in range(n-1, mid-1, -1):
+        new_arr[j] = arr[i]
+        j += 2
+    
+    j = 1
+    for i in range(0, mid, 1):
+        new_arr[j] = arr[i]
+        j += 2
+    
+    print(new_arr)
+
+
+
 if __name__ == "__main__":
     # print("\n")
     # arr = [5, 10, 15, 2, 3, 9, 7, 8, 11]
@@ -26,12 +52,20 @@ if __name__ == "__main__":
     # LargestSmallest(arr, n)
     # print("\n")
 
-    n = int(input("\nEnter the Array Size: "))
-    arr = list(map(int, input("Enter the Array Elements: ").split()))[:n]
-    res = LargestSmallest(arr, n)
-    print("\nSorted List: ", end = " ")
-    for i in range(len(res)):
-        print(res[i], end = " ")
+
+    print("\n")
+    # arr = [37, 12, 33, 9, 43, 19, 20]
+    arr = [5, 10, 15, 2, 3, 9, 7, 8, 11]
+    n = len(arr)
+    LargeSmall2(arr, n)
+    print("\n")
+
+    # n = int(input("\nEnter the Array Size: "))
+    # arr = list(map(int, input("Enter the Array Elements: ").split()))[:n]
+    # res = LargestSmallest(arr, n)
+    # print("\nSorted List: ", end = " ")
+    # for i in range(len(res)):
+    #     print(res[i], end = " ")
     print("\n")
 
 
